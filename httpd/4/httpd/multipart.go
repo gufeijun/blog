24430a5bfc14399edb304d23ca31017073e0c5a7
gufeijun <GuGoing@outlook.com>
2021-06-13 14:27:10 +0800

    add an example

--- httpd/4/httpd/multipart.go
+++ httpd/4/httpd/multipart.go
@@ -9,6 +9,39 @@ import (
 	"strings"
 )
 
+//multipart/form-data example:
+//POST / HTTP/1.1
+//[[ Less interesting headers ... ]]
+//Content-Type: multipart/form-data; boundary=---------------------------735323031399963166993862150
+//Content-Length: 834
+//
+//-----------------------------735323031399963166993862150
+//Content-Disposition: form-data; name="text1"
+//
+//text default
+//-----------------------------735323031399963166993862150
+//Content-Disposition: form-data; name="text2"
+//
+//aωb
+//-----------------------------735323031399963166993862150
+//Content-Disposition: form-data; name="file1"; filename="a.txt"
+//Content-Type: text/plain
+//
+//Content of a.txt.
+//
+//-----------------------------735323031399963166993862150
+//Content-Disposition: form-data; name="file2"; filename="a.html"
+//Content-Type: text/html
+//
+//<!DOCTYPE html><title>Content of a.html.</title>
+//
+//-----------------------------735323031399963166993862150
+//Content-Disposition: form-data; name="file3"; filename="binary"
+//Content-Type: application/octet-stream
+//
+//aωb
+//-----------------------------735323031399963166993862150--
+
 const bufSize = 4096
 
 type MultipartReader struct {
